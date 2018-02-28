95a8e8fbe13600d926e8f1e0bb2043af7e78723d
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-28 03:42:52 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,16 @@
 module Memorable
+  module ClassMethods
   def reset_all
     self.all.clear
   end
 
   def self.count
     self.all.length
+  end
+
+  module InstanceMethods
+    def initialize
+    end
   end
 
 end
